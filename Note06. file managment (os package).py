832d18a0c0073545e4ad562ccd7faa703d432f60
Derrick Lau <derrick.evltfmr@gmail.com>
2019-06-09 23:50:28 +0800

    Update 'os.path' part

--- Note06. file managment (os package).py
+++ Note06. file managment (os package).py
@@ -39,4 +39,24 @@ os.system("mkdir newdirectory")                                          # make 
 os.system("copy myfile.txt newdirectory/copyfile.txt")                   # copy a file
 
 copyfile = current_path + "/newdirectory/copyfile.txt"
-os.system("notepad++ " + copyfile)                                           # open the file with notepad++
+os.system("notepad++ " + copyfile)                                       # open the file with notepad++
+
+
+# os.path (part of the os package)
+# // os.path can help to deal with the file path and name, to check whether the file or path exists, and calculate the size of the file
+import os.path
+someFile = "myfile.txt"
+somePath = "C:/somedirectory/"
+os.path.abspath(someFile)                                                # return the complete path of the file
+os.path.dirname(someFile)                                                # return the complete path of the directory of the file
+os.path.dirname(__file__)                                                # return the complete path of the directory of the current file (__file__)
+os.path.exists(someFile)                                                 # check whether the file exists
+os.path.getsize(someFile)                                                # get the size of the file
+
+os.path.isabs(somePath)                                                  # check whether the path is a complete path
+os.path.isfile(somePath)                                                 # check whether the path is a file
+os.path.isdir(somePath)                                                  # check whether the path is a directory
+
+os.path.split(somePath)                                                  # split the path to directory path and file
+os.path.splitdrive(somePath)                                             # split the path to the drive and the file path
+os.path.join(somePath + "/" + someFile)                                  # join/group the path and the file to a complete path        
