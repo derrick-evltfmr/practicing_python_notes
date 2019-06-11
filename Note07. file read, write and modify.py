9eec1b97057f4dc31f09c5b1eaf5f907dd363c2c
Derrick Lau <derrick.evltfmr@gmail.com>
2019-06-11 13:19:39 +0800

    - Updated Note07. file read, write and modify

--- Note07. file read, write and modify.py
+++ Note07. file read, write and modify.py
@@ -69,3 +69,40 @@ myFile.write(content)                                                   # write 
 
 myFile.flush()                                                          # when the file closes, it will write the data into the file, but we can also use flush() to force                                                                        # the data in the buffer zone to be written into the file instantly
 myFile.close()                                                          # after the file closes, it can no longer do read and write actions
+
+
+# BOM character (important to know)
+### // this's important because it can cause big errors if the data is read in incorrectly !!! ###
+
+# // see the following example, readlines() can return all the content in the document, including the hidden characters
+# > write the file first
+test = open('test.txt','w', encoding='UTF-8')
+content = """
+Hello Python 
+"""
+test.write(content)
+test.close()
+
+# > read the file above
+with open('test.txt', 'r', encoding = 'UTF-8') as f:
+    doc = f.readlines()
+    print(doc)                                                          # \ufeffHello Python
+
+f = open('text.txt', 'r', encoding ='UTF-8')
+str1=f.read(5)
+print(str1)                                                             # Hell
+f.close()
+
+# // reading in the file for 5 characters, shouldn't it print out "Hello", not "Hell"?
+# // it's because in readlines(), we can see there's a character \ufeff, it is called the BOM character and it's generated automatically when using UTF-8 format
+# // and it will occupy 1 character, therefore, '\ufeff' is the 1st character, and "Hell" are the 2nd to 5th character
+# // this's important because it can cause big errors if the data is read in incorrectly !!! #
+
+# // with some document editor like NotePad++, we can choose "Encode in UTF-8 (no BOM in the beginning)" to remove BOM character
+
+# // Another very useful way is to add '-sig' after 'UTF-8' when reading in a file
+with open('test.txt', 'r', encoding = 'UTF-8-sig') as f:
+    doc = f.readlines()
+    print(doc)                                                          # Hello Python
+
+# // with this way, using 'UTF-8-sig', we can read in the files without the BOM character
