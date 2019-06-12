c097baf0ad6e1cee1909da24b20289915559fa9b
Derrick Lau <derrick.evltfmr@gmail.com>
2019-06-12 19:37:20 +0800

    Updated 'example(menu and readData)' part

--- Note08. file application and SQLite.py
+++ Note08. file application and SQLite.py
@@ -7,11 +7,34 @@ data = {"derrick":"123123", "john":"456456", "charles":"789789"}
 with open('password.txt','w', encoding = 'UTF-8-sig') as f:
     f.write(data)                                                        # the data is dict, however, after written in the txt file, it is just simply string, not dict
 
-# ast package and literal_eval()
+# ast package and literal_eval() to convert string to list/dict
 import ast                                                               # ast package (Abstract Syntax Tree)
 data = dict()
 with open('password.txt', 'r', encoding = 'UTF-8-sig') as f:
     filedata = f.read()                                                  # file read the string "{"derrick":"123123", "john":"456456", "charles":"789789"}"
     data = ast.literal_eval(filedata)                                    # using literal_eval() function in ast package to convert the string to list or dict
                                                                          # now data = {"derrick":"123123", "john":"456456", "charles":"789789"}
+
+# >>> example of a simple Account/Password Management System using the file related functions (from the book)
+import os
+def menu():
+    os.system("cls")
+    print("Account Password Management System")
+    print("----------------------------------")
+    print("1. Enter account and password")
+    print("2. display account and password")
+    print("3. modify password")
+    print("4. delete account and password")
+    print("0. end program")
+    print("----------------------------------")
+
+def readData():
+    with open('password.txt', 'r', encoding = 'UTF-8-sig') as f:
+        filedata = f.read()                                              # read data from the file into filedata
+        if filedata != "":                                               # if the filedata is not empty (the data read from the file is not empty)
+            data = ast.literal_eval(filedata)                            # convert the filedata(string type) to data (dict type)
+            return data                                                  # return data
+        else: 
+            return dict()                                                # if the filedata is empty, return empty dict
+
 
